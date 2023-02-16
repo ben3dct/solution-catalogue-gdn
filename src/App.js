f33d9c027594292167e3a1f6268acb92133caefa
ben3dct <yennexl@gmail.com>
2023-02-16 14:09:26 +0100

    remastered routes and UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 /** @format */
 
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth } from "aws-amplify";
+import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth";
 import awsmobile from "./aws-exports";
+
+import * as React from "react";
 
 Amplify.configure({
 	...awsmobile,
@@ -18,16 +20,40 @@ Amplify.configure({
 });
 
 function App() {
+	const [user, setUser] = React.useState(null);
+	const [loader, setLoader] = React.useState(true);
+
+	function updateUser(data) {
+		setUser(data);
+	}
+	async function getUser() {
+		Auth.currentAuthenticatedUser()
+			.then((data) => {
+				setUser(data);
+				console.log("setuser");
+			})
+			.catch((e) => {
+				console.log("no authenticated user");
+			});
+	}
+	React.useEffect(() => {
+		getUser();
+	}, []);
+	React.useEffect(() => {
+		console.log(user);
+		setLoader(false);
+	}, [user]);
+
 	return (
 		<>
 			<Routes>
 				<Route
 					path='/'
-					element={<Home />}
+					element={!loader ? <Home user={user} /> : <h1>loading</h1>}
 				/>
 				<Route
 					path='/login'
-					element={<Login />}
+					element={<LoginPage updateUser={updateUser} />}
 				/>
 			</Routes>
 		</>
@@ -35,3 +61,11 @@ function App() {
 }
 
 export default App;
+
+const LoginPage = ({ updateUser }) => {
+	async function signIn() {
+		await Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google });
+		// eslint-disable-next-line no-restricted-globals
+	}
+	return <button onClick={signIn}>sign in</button>;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,79 @@
 
 import { signIn, signOut } from "./../Auth";
 
-const Home = () => {
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./Home.styles.css";
+import { Auth } from "aws-amplify";
+import { data } from "./data";
+import * as React from "react";
+const Home = ({ user }) => {
+	const [selectedItem, setSelectedItem] = React.useState(null);
+	const [filterValue, setFilterValue] = React.useState(null);
+	const [filterResult, setFilterResult] = React.useState(data);
+	function updateSelectedItem(id) {
+		if (id === selectedItem) {
+			return {
+				fontWeight: "bolder",
+				border: "2px solid black",
+				boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+				marginTop: "5px",
+				marginBottom: "15px",
+			};
+		} else {
+			return {};
+		}
+	}
+
+	React.useEffect(() => {
+		if (filterValue) {
+			const result = data.filter((datum) => datum.id == filterValue);
+			setFilterResult(result);
+		} else {
+			setFilterResult(data);
+		}
+	}, [filterValue]);
+	if (!Auth.currentAuthenticatedUser()) {
+		return <Navigate to='/login' />;
+	}
 	return (
-		<div>
-			<button onClick={signIn}>sign in</button>
+		<div className='primary-flex'>
+			<div className='flex-child header'>header placeholder</div>
+			<div className='flex-child filters'>
+				<input
+					placeholder='search id'
+					onChange={(e) => {
+						setFilterValue(e.target.value);
+					}}
+				/>
+				<span> filtering for id: {filterValue ? filterValue : "all "}</span>
+			</div>
+			<div className='flex-child body'>
+				{filterResult.map((datum) => {
+					return (
+						<div
+							style={updateSelectedItem(datum.id)}
+							onClick={() => {
+								setSelectedItem(datum.id);
+								updateSelectedItem(datum.id);
+							}}
+							className='map-item'
+							key={datum.id}>
+							{datum.id}
+						</div>
+					);
+				})}
+			</div>
+			<div className='flex-child footer'>
+				Status: Disconected, Selected Item: {selectedItem ? selectedItem : "none"}
+				<button
+					disabled={selectedItem ? false : true}
+					style={{ marginLeft: "20px" }}
+					onClick={() => {
+						setSelectedItem(null);
+					}}>
+					deselect item
+				</button>
+			</div>
 		</div>
 	);
 };
