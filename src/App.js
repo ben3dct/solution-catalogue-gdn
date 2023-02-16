af3af9f1b8e22a6a7ec4a2b42617fa2de8ae5177
ben3dct <yennexl@gmail.com>
2023-02-16 12:44:59 +0100

    add auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+
+import { Amplify } from "aws-amplify";
+import awsmobile from "./aws-exports";
+
+Amplify.configure({
+	...awsmobile,
+	oauth: {
+		...awsmobile.oauth,
+		redirectSignIn: awsmobile.oauth.redirectSignIn.split(",")[+!window.location.hostname.includes("localhost")],
+		redirectSignOut: awsmobile.oauth.redirectSignOut.split(",")[+!window.location.hostname.includes("localhost")],
+	},
+});
+
 function App() {
 	return (
 		<>
