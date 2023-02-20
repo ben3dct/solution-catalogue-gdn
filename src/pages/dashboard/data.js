fee73424d193640d5dd7ed8cc76a56a6cd2a90f0
ben3dct <yennexl@gmail.com>
2023-02-20 13:23:28 +0100

    add more mock data

--- src/pages/dashboard/data.js
+++ src/pages/dashboard/data.js
@@ -3,8 +3,196 @@
 export const data = [
 	{
 		id: 1,
+		title: "Compliance Dashboards",
+		description:
+			"Creates a Lambda that provides compliance dashboards as HTML snippets. Organizations and Config are used as data sources.",
+		author: "David Krohn",
+		tags: "Security",
 	},
 	{
 		id: 2,
+		title: "Handling file hash as base64",
+		description: "Read file and get SHA256 checksum.",
+		author: "Stephan Huber",
+		tags: "Storage",
+	},
+	{
+		id: 3,
+	},
+	{
+		id: 4,
+	},
+	{
+		id: 5,
+	},
+	{
+		id: 6,
+	},
+	{
+		id: 7,
+	},
+	{
+		id: 8,
+	},
+	{
+		id: 9,
+	},
+	{
+		id: 10,
+	},
+	{
+		id: 11,
+	},
+	{
+		id: 12,
+	},
+	{
+		id: 13,
+	},
+	{
+		id: 14,
+	},
+	{
+		id: 15,
+	},
+	{
+		id: 16,
+	},
+	{
+		id: 17,
+	},
+	{
+		id: 18,
+	},
+	{
+		id: 19,
+	},
+	{
+		id: 13,
+	},
+	{
+		id: 14,
+	},
+	{
+		id: 15,
+	},
+	{
+		id: 16,
+	},
+	{
+		id: 17,
+	},
+	{
+		id: 18,
+	},
+	{
+		id: 19,
+	},
+	{
+		id: 13,
+	},
+	{
+		id: 14,
+	},
+	{
+		id: 15,
+	},
+	{
+		id: 16,
+	},
+	{
+		id: 17,
+	},
+	{
+		id: 18,
+	},
+	{
+		id: 19,
+	},
+	{
+		id: 13,
+	},
+	{
+		id: 14,
+	},
+	{
+		id: 15,
+	},
+	{
+		id: 16,
+	},
+	{
+		id: 17,
+	},
+	{
+		id: 18,
+	},
+	{
+		id: 19,
+	},
+
+	{
+		id: 13,
+	},
+	{
+		id: 14,
+	},
+	{
+		id: 15,
+	},
+	{
+		id: 16,
+	},
+	{
+		id: 17,
+	},
+	{
+		id: 18,
+	},
+	{
+		id: 19,
+	},
+	{
+		id: 13,
+	},
+	{
+		id: 14,
+	},
+	{
+		id: 15,
+	},
+	{
+		id: 16,
+	},
+	{
+		id: 17,
+	},
+	{
+		id: 18,
+	},
+	{
+		id: 19,
+	},
+
+	{
+		id: 13,
+	},
+	{
+		id: 14,
+	},
+	{
+		id: 15,
+	},
+	{
+		id: 16,
+	},
+	{
+		id: 17,
+	},
+	{
+		id: 18,
+	},
+	{
+		id: 19,
 	},
 ];
